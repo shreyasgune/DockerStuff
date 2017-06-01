26d7952fd31df09dcb3b445aa8def69b4cb64a2b
Shrèyas Gunè <sgune@gannett.com>
2017-06-01 12:45:26 -0400

    Update install.sh

--- dockanode-code/install.sh
+++ dockanode-code/install.sh
@@ -5,7 +5,7 @@ sudo /usr/bin/python27 /tmp/ez_setup.py
 sudo /usr/bin/easy_install-2.7 pip 
 sudo pip install ansible
 curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose 
-
+sudo chmod +x /usr/local/bin/docker-compose
 # should display current versions:
 pip -V 
 docker-compose --version
